f21563d57b96db940e8a374a84d05df5a967ba73
dznz <daniel.zollinger@gmail.com>
2011-01-12 14:15:04 +1300

    added some manual steps notifications, added cucumber install

--- base.rb
+++ base.rb
@@ -3,11 +3,30 @@ if yes?("Do you want to use Mongoid?")
   gem "mongoid", "2.0.0.rc.1"
   gem "bson_ext"
   generate "mongoid:config"
+puts <<MANUAL_STEPS
+
+== Remember To ==
+replace "require \'rails/all\'" with the following
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "active_resource/railtie"
+
+MANUAL_STEPS
 end
 
-if yes?("Do you want to use Rspec?")
+if yes?("Do you want to use Cucumber and Rspec?")
+  gem 'cucumber-rails', :group => [:development, :test]
+  gem 'capybara', :group => [:development, :test]
+  #gem 'database_cleaner', "0.6.0", :group => [:development, :test]
   gem 'rspec-rails', :group => [:development, :test]
+  generate "cucumber:install", "--rspec --capybara --skip-database"
   generate "rspec:install"
+puts <<MANUAL_STEPS
+
+== Remember To ==
+Remove config.fixture_path and config.use_transactional_fixtures lines from spec_helper.rb if using Mongoid
+
+MANUAL_STEPS
 end
 
 # Git initialisation
